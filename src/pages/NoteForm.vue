76661ae11945c91388dda431ba13c2affccf8025
JesseMito <rhadamessousa@gmail.com>
2022-01-31 16:09:20 -0300

    add validation, and call addNewNote function

--- src/pages/NoteForm.vue
+++ src/pages/NoteForm.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="container">
     <h2 class="text-center">New Note</h2>
-    <form>
+    <p v-if="!isValid">Please enter valid values</p>
+    <form @submit.prevent="saveNote" novalidate>
       <div class="mb-3">
         <label for="title">Title: </label>
-        <input class="form-control" type="text" id="title" v-model="title" />
+        <input
+          class="form-control"
+          :class="
+            wasValidated ? (titleIsValid ? 'is-valid' : 'is-invalid') : ''
+          "
+          type="text"
+          id="title"
+          v-model="title"
+        />
       </div>
       <div class="mb-3">
         <label for="content">Content: </label>
         <textarea
           class="form-control"
+          :class="
+            wasValidated ? (contentIsValid ? 'is-valid' : 'is-invalid') : ''
+          "
           v-model="content"
           id="content"
         ></textarea>
@@ -22,18 +34,67 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, inject, ref } from 'vue';
 import Navbar from '../components/Navbar.vue';
+import { Note } from '../types';
+
+type NoteFormInject = (note: Note) => void;
 
 export default defineComponent({
   name: 'NoteForm',
   components: {
     Navbar,
   },
-  data() {
+  // It uses the Composition API, since it's not possible to use provide/inject with typescript in the Options API
+  setup() {
+    // Form Values
+    const title = ref('');
+    const content = ref('');
+
+    // Form Validation
+    const titleIsValid = ref(false);
+    const contentIsValid = ref(false);
+    // Checks if everything is valid
+    const isValid = ref(true);
+    // Checks if it was validated, since I don't want it to validate until the save button is pressed
+    const wasValidated = ref(false);
+
+    const addNewNote = inject<NoteFormInject>('addNewNote');
+    const saveNote = () => {
+      // Validation
+      wasValidated.value = true;
+
+      // Checks one by one
+      titleIsValid.value = title.value.length > 0;
+      contentIsValid.value = content.value.length > 0;
+
+      // Checks evertything
+      isValid.value = titleIsValid.value && contentIsValid.value;
+
+      // Type guard for function and validation
+      if (!addNewNote || !isValid.value) return;
+
+      addNewNote({
+        title: title.value,
+        content: content.value,
+        date: new Date(),
+      });
+
+      title.value = '';
+      content.value = '';
+
+      // Removes validation from the UI
+      wasValidated.value = false;
+    };
+
     return {
-      title: '',
-      content: '',
+      saveNote,
+      title,
+      content,
+      titleIsValid,
+      contentIsValid,
+      isValid,
+      wasValidated,
     };
   },
 });
