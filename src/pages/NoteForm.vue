22c32f366df794f17e3276c57adf1fdfa3cd0bba
JesseMito <rhadamessousa@gmail.com>
2022-01-31 13:33:50 -0300

    add note form page

---
+++ src/pages/NoteForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <Navbar />
+
+  <div class="container">
+    <h2 class="text-center">New Note</h2>
+    <form>
+      <div class="mb-3">
+        <label for="title">Title: </label>
+        <input class="form-control" type="text" id="title" v-model="title" />
+      </div>
+      <div class="mb-3">
+        <label for="content">Content: </label>
+        <textarea
+          class="form-control"
+          v-model="content"
+          id="content"
+        ></textarea>
+      </div>
+      <div class="mb-3 d-flex justify-content-end">
+        <button class="btn btn-primary">Save</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Navbar from '../components/Navbar.vue';
+
+export default defineComponent({
+  name: 'NoteForm',
+  components: {
+    Navbar,
+  },
+  data() {
+    return {
+      title: '',
+      content: '',
+    };
+  },
+});
+</script>
+
+<style scoped>
+#content {
+  min-height: 400px;
+}
+</style>
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './pages/Home.vue';
+import NoteForm from './pages/NoteForm.vue';
 
 const routes = [
   {
     path: '/',
     component: Home,
+  },
+  {
+    path: '/new-note',
+    component: NoteForm,
   },
 ];
 
