6576e8cc45fcbb9ade5a55cd495ef360eb1237cb
JesseMito <rhadamessousa@gmail.com>
2022-01-31 10:16:51 -0300

    create pages directory

---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <Navbar />
+  <NoteList />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Navbar from '../components/Navbar.vue';
+import NoteList from '../components/NoteList.vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    Navbar,
+    NoteList,
+  },
+});
+</script>
