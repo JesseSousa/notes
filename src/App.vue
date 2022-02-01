8450870ac841107e962be4e366141211acc27698
JesseMito <rhadamessousa@gmail.com>
2022-02-01 15:03:43 -0300

    load notes from database

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,19 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { mapActions } from 'vuex';
 import Navbar from './components/Navbar.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     Navbar,
+  },
+  methods: {
+    ...mapActions(['loadNotes']),
+  },
+  mounted() {
+    this.loadNotes();
   },
 });
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -16,17 +16,35 @@ export default createStore<State>({
     addNote(state, note: Note) {
       state.notes.push(note);
     },
+    setNotes(state, notes: Note[]) {
+      state.notes = notes;
+    },
   },
   actions: {
     addNote(context, note: Note) {
+      // Date is saved as a string in the database, since firestore doesn't support the javascript Date object
+
       db.collection('notes')
-        .add(note)
+        .add({ ...note, date: note.date.toString() })
         .then((docRef) => {
           console.log('Document written with ID: ', docRef.id);
           context.commit('addNote', { ...note, id: docRef.id });
         })
         .catch((error) => {
           console.error('Error adding document: ', error);
+        });
+    },
+    loadNotes(context) {
+      db.collection('notes')
+        .get()
+        .then((snapshot) => {
+          const notes = snapshot.docs.map((doc: any) => ({
+            ...doc.data(),
+            id: doc.id,
+            date: new Date(doc.data().date),
+          }));
+
+          context.commit('setNotes', notes);
         });
     },
   },
