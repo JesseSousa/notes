c63c11c5797f873b3f51f338bfe4d78edb92db57
JesseMito <rhadamessousa@gmail.com>
2022-02-11 07:41:13 -0300

    use vueDomPurify extension

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import router from './routes';
 import store from './store';
 import App from './App.vue';
+import VueDOMPurify from 'vue-dompurify-html';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
@@ -9,5 +10,6 @@ const app = createApp(App);
 
 app.use(router);
 app.use(store);
+app.use(VueDOMPurify);
 
 app.mount('#app');
